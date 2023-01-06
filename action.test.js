b977308e7d8e807fd5de828a7dd8ab3aca1f52ca
Jeff Glatz <jeff@bestquality.engineering>
2023-01-05 22:08:19 -0600

    Extract testable function

--- action.test.js
+++ action.test.js
@@ -1,18 +1,16 @@
 const process = require("process");
 const child = require("child_process");
 const path = require("path");
+const run = require("./action");
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test("should execute command with only goals specified", () => {
+test("should execute command with only goals specified", async () => {
     process.env["INPUT_ERRORS"] = "true";
     process.env["INPUT_BATCH_MODE"] = "true";
     process.env["INPUT_NO_TRANSFER_PROGRESS"] = "true";
-    process.env["INPUT_PROFILES"] = "undefined";
     process.env["INPUT_REVISION"] = "undefined";
     process.env["INPUT_SHA1"] = "undefined";
     process.env["INPUT_CHANGELIST"] = "undefined";
     process.env["INPUT_GOALS"] = "clean install";
-    const script = path.join(__dirname, "index.js");
-    const result = child.execSync(`node ${script}`, {env: process.env}).toString();
-    console.log(result);
+    await run();
 })
--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-const core = require("@actions/core");
-const exec = require("@actions/exec");
-const MavenArguments = require("./MavenArguments")
+const action = require("./action")
 
-async function run() {
-    try {
-        await exec.exec("mvn", new MavenArguments()
-            // boolean options
-            .withErrors(core.getBooleanInput("errors", {required: true}))
-            .withBatchMode(core.getBooleanInput("batch_mode", {required: true}))
-            .withNoTransferProgress(core.getBooleanInput("no_transfer_progress", {required: true}))
-            // CI-friendly properties
-            .withRevisionProperty(core.getInput("revision", {required: true}))
-            .withSha1Property(core.getInput("sha1", {required: true}))
-            .withChangelistProperty(core.getInput("changelist", {required: true}))
-            // Options
-            .withFile(core.getInput("file", {required: false}))
-            .withProfiles(core.getInput("profiles", {required: false}))
-            .withProjects(core.getInput("projects", {required: false}))
-            .withSettings(core.getInput("settings", {required: false}))
-            .withToolchains(core.getInput("toolchains", {required: false}))
-            .withThreads(core.getInput("threads", {required: false}))
-            // Goals
-            .withGoals(core.getInput("goals", {required: false}))
-            // Phases
-            .withPhases(core.getInput("phases", {required: false}))
-            .toArray())
-    } catch (error) {
-        core.setFailed(error.message);
-    }
-}
-
-run();
+action();
